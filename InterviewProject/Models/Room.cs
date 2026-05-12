254eb7748dbad702e12fe42c03961b89091e5b5d
盧品卉 <109534239@stu.ukn.edu.tw>
2026-05-13 06:25:32 +0800

    room

--- InterviewProject/Models/Room.cs
+++ InterviewProject/Models/Room.cs
@@ -1,12 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace InterviewProject.Models
+﻿namespace InterviewProject.Models
 {
     public class Room
     {
         public int Id { get; set; }
-
-        [Required]
         public string RoomName { get; set; }
         public DateTime CreatedTime { get; set; }
     }
